7fa11fe35699219016a14016bfd3a9305695f65a
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-08-11 12:47:47 -0300

    Adicionei uma constante que irá receber o readline para a intereção com o usuário.

---
+++ script.js
@@ -0,0 +1,13 @@
+import chalk from 'chalk';
+import readline from 'readline';
+
+
+let itens = []
+
+const leia = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
